b8cea31437e56b4f16028bc3feafd7ba3ec8fa3d
danbrong <bchang91@gmail.com>
2023-03-28 12:24:37 -0700

    Completed initial index and javascript projects

---
+++ Basic_Javascript/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,27 @@
+
+// VARIABLE DECLARATION
+var A = "This is the window variable string ";
+var B = "This is a variable string ";
+var C = "Concated " + "String ";
+var D = "Colors ", E = "Red ", F = "Blue ", G = "Yellow ";
+var Sent1 = "This is the beginning of the string, ", Sent2 = "and this is the end of the string. ";
+// END VARIABLE DECLARATION
+
+window.alert("Hello, World!"); // Creates Hello, World Pop-Up
+window.alert(A); // Created a popup using a variable.
+
+document.write(B); // Print Variable text
+
+document.write("Abraham Lincoln once said, \"Don't always believe everything you read on the internet.\", a cautionary tale. "); // Test for escaping quotation marks.
+
+document.write("This is some concatenated text, "
++ "Part 2 of string text, "
++ "Part 3 of string text. "); // Demonstrating concatenated document function
+
+document.write(C); // Demonstrating the write function with a concatenated variable
+
+document.write(E); // Demonstrating multiple defined variables in one statement
+
+document.write(6*8); // Demonstrating an expression
+
+document.write(Sent1+Sent2); //Demonstrating Concatinating Variables
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Javascript_Projects
+### Javascript_Projects
 Tech Academy Javascript Projects
+
+
+### Introduction: 
+This repository was established to house all projects relating to the Tech Academy's "Javascript" Course. As projects are completed, they will be stored in this repository.
+
+### Technologies:
+Javascript - Programming language to create dynamic webpages
+HTML - Language for website structure
+CSS - Language for website appearances
+Git - Manages repository projects
+GitHub - Houses and controls our repository
+
+### Launch:
+
+
+### Illustrations:
+
+### Examples of Use:
+
+### Project Status:
+
+### Sources:
+
+### Other Information:
