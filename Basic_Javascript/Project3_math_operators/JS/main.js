e652b001b54e1efdcd9b4f83af113748cc005b97
danbrong <bchang91@gmail.com>
2023-03-29 14:21:06 -0700

    Created Mathematical Operators Project

---
+++ Basic_Javascript/Project3_math_operators/JS/main.js
@@ -0,0 +1,62 @@
+function myAddition(x,y) {
+    return x + y;
+}
+// Function to add
+
+function mySubtraction(x,y) {
+    return x - y;
+}
+// Function to subtract
+
+function myMultiply(x,y) {
+    return x * y;
+}
+// Function to multiply
+
+function myDivide(x,y) {
+    return x / y;
+}
+// Function to divide
+
+function myMath(a,b,c,d,e) {
+    return a / b * c + d - e ;
+}
+// Function to do multiple operations
+
+function myModulus(x,y) {
+    return x % y;
+}
+// Function to find remainder
+
+function myNegative(x) {
+    return -x;
+}
+// Function to make a number negative
+
+function myIncrement(X) {
+    X++;
+    document.write(X)
+}
+// Function to increment by 1
+
+function myDecrement(Y) {
+    Y--;
+    document.write(Y)
+}
+// Function to decrement by 1
+
+function myRandom() {
+    random = (Math.random() * 10);
+    return random;
+}
+// Function to get a random number between 0-10
+
+function myPi() {
+    return Math.PI;
+}
+// Function to call Pi
+
+function myRound(x) {
+    return Math.round(x);
+}
+// Function to round a number
